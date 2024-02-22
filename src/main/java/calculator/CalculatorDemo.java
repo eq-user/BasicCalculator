eccca4098e9e7a7f190f4808c23c503139af0123
eq-user <160815718+eq-user@users.noreply.github.com>
2024-02-22 11:13:44 +0530

    Update version string to 1.0.3

--- src/main/java/calculator/CalculatorDemo.java
+++ src/main/java/calculator/CalculatorDemo.java
@@ -5,7 +5,7 @@ public class CalculatorDemo
 
 	public static void main() 
 	{
-		System.out.println("This is simple CalculatorDemo program");
+		System.out.println("This is simple CalculatorDemo program. Version - 1.0.3 ");
 
 	}
 
